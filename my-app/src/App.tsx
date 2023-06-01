5f67d430c5e756f7f6afe814ab66d297588b2c57
caushcani <caushcani@gmail.com>
2023-06-01 21:49:43 +0200

    fix: remove value from withdraw function

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import TsxManager from "./utils/TsxManager";
 import useWeb3Provider from "./hooks/useWeb3Provider";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const { web3Provider, contract } = useWeb3Provider();
@@ -10,12 +12,44 @@ function App() {
   const [balance, setBalance] = useState(0);
   const ADDR = "0xf9570CBf7E7B2B7Caf97f1b5cfD61535B0A5eC0d";
 
+  const [triggerReload, setTriggerReload] = useState(false);
+
+  const withdrawNotify = () => {
+    toast.success("🦄 Withdraw successfully!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      theme: "light",
+    });
+  };
+
+  const depositNotify = () => {
+    toast.success("🦄 Deposit successfully!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+
+      theme: "light",
+    });
+  };
+
   const getAccounts = async () => {
     if (web3Provider?.web3) {
       const acc = await web3Provider.web3?.eth.getAccounts();
       if (acc) {
         setAccount(acc[0]);
       }
+    }
+  };
+
+  const getBalance = async () => {
+    if (web3Provider.web3) {
       const contractB = await web3Provider.web3.eth.getBalance(ADDR);
       if (contractB) {
         setBalance(contractB);
@@ -31,17 +65,18 @@ function App() {
       );
       const res = await newContract.methods.withdraw().send({
         from: account,
-        value: "10000000000000000000",
+        // value: "10000000000000000000",
       });
-      // const res = await web3Provider.web3.eth.sendTransaction({
-      //   from: ADDR,
-      //   to: account,
-      //   value: "1000000000000000000",
-      // });
-
-      console.log("RES", res);
+      if (res) {
+        withdrawNotify();
+      }
+      setTriggerReload(!triggerReload);
     }
   };
+
+  useEffect(() => {
+    getBalance();
+  }, [triggerReload]);
 
   const donate = async () => {
     if (contract && web3Provider.web3) {
@@ -50,14 +85,18 @@ function App() {
         to: ADDR,
         value: "10000000000000000000",
       });
-
-      console.log("RES", res);
+      if (res) {
+        depositNotify();
+      }
+      setTriggerReload(!triggerReload);
     }
   };
   useEffect(() => {
     if (web3Provider.web3) {
       getAccounts();
     }
+
+    setTriggerReload(!triggerReload);
   }, [web3Provider.web3]);
 
   return (
@@ -81,6 +120,7 @@ function App() {
             <button className="btn btn-danger" onClick={() => handleWithdraw()}>
               Withdraw
             </button>
+            <ToastContainer />
           </div>
         </div>
       </div>
